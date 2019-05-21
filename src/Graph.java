a526123f386f480963f21845bd012e05894c62a8
Sachin Joshi <30396084+saj415@users.noreply.github.com>
2019-05-21 12:09:09 -0400

    Create Graph.java

---
+++ src/Graph.java
@@ -0,0 +1,14 @@
+/**
+ * Mars Rover Solution
+ * @author Sachin Joshi
+ */
+
+import java.util.*;
+
+class Graph {
+	HashMap<String, ArrayList<String>> adjlist;
+	
+	Graph() {
+		adjlist = new HashMap<String, ArrayList<String>>();
+	}
+}
